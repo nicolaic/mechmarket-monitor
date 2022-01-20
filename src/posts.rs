56355a3c904235a8b3cf48da3b0f0096ffff53b2
Nicolai <contact@nicolai.dev>
2022-01-20 07:46:45 +0100

    Implement std::fmt::Display for PostTitle and move parse_post_title to PostTitle::new

--- src/posts.rs
+++ src/posts.rs
@@ -1,12 +1,13 @@
-use regex::Regex;
+use std::fmt;
+
 use lazy_static::lazy_static;
+use regex::Regex;
 
 lazy_static! {
     static ref TITLE_REGEX: Regex = Regex::new(r"^\[(?P<country>..)(?:-(?P<state>..))?\]\s*\[H\](?P<have>.*)\[W\](?P<want>.*)$").unwrap();
     static ref COMMA_SEPARATED_REGEX: Regex = Regex::new(r"\s*,\s*").unwrap();
 }
 
-#[derive(Debug)]
 pub struct PostTitle<'t> {
     country: &'t str,
     state: Option<&'t str>,
@@ -14,15 +15,24 @@ pub struct PostTitle<'t> {
     want: Vec<&'t str>,
 }
 
-pub fn parse_post_title(post_title: &str) -> Result<PostTitle, Box<dyn std::error::Error>> {
-    let captures = TITLE_REGEX.captures(post_title).ok_or("Failed to parse post title")?;
+impl fmt::Display for PostTitle<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let state = self.state.map(|state| format!("-{}", state)).unwrap_or_default();
+        write!(f, "[{}{}] [H] {} [W] {}", self.country, state, self.have.join(", "), self.want.join(", "))
+    }
+}
 
-    Ok(PostTitle {
-        country: captures.name("country").unwrap().as_str(),
-        state: captures.name("state").map(|m| m.as_str()),
-        have: split_items(captures.name("have").unwrap().as_str().trim()),
-        want: split_items(captures.name("want").unwrap().as_str().trim()),
-    })
+impl PostTitle<'_> {
+    pub fn new(post_title: &str) -> Option<PostTitle> {
+        let title_captures = TITLE_REGEX.captures(post_title);
+
+        title_captures.map(|captures| PostTitle {
+            country: captures.name("country").unwrap().as_str(),
+            state: captures.name("state").map(|m| m.as_str()),
+            have: split_items(captures.name("have").unwrap().as_str().trim()),
+            want: split_items(captures.name("want").unwrap().as_str().trim()),
+        })
+    }
 }
 
 fn split_items(items: &str) -> Vec<&str> {
